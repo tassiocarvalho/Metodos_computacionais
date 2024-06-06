a8f2d4fb36c1d5ce206bb5a2e46f2b062c8aec57
Tassio Carvalho <tassiocarvalhor@gmail.com>
2024-06-06 18:24:37 -0300

    sor ok

--- Unidade02/Avaliação02/SOR_Jacobi_seiidel/sor_method.m
+++ Unidade02/Avaliação02/SOR_Jacobi_seiidel/sor_method.m
@@ -2,18 +2,17 @@ function sor_method()
     % Definindo a matriz A e o vetor b
     A = [-17/105, 1/30, 0, 1/10; 1/30, -253/840, 1/8, 1/7; 0, 1/8, -23/120, 1/15; 1/10, 1/7, 1/15, -107/210];
     b = [-20/35; 0; 0; -28];
-    
-    % Parâmetros iniciais
-    n = length(b);
-    x = zeros(n, 1); % Vetor inicial
-    omega = 0.19; % Valor de omega
-    max_iter = 100;
-    tol = 0.01; % Critério de parada (erro relativo 10%)
-    div_tol = 1e-6; % Tolerância para verificar não convergência
-    stagnation_iter = 10; % Número de iterações para verificar estagnação
 
-    % Iterando com o valor fixo de omega
-    previous_errors = ones(stagnation_iter, 1) * inf; % Inicializando erros anteriores para verificar estagnação
+    % Parâmetros iniciais
+    n = length(b); % Determina o número de variáveis
+    x = zeros(n, 1); % Vetor inicial de zeros
+    omega = 0.19; % Fator de relaxamento
+    max_iter = 1000; % Máximo de iterações permitidas
+    tol = 0.0001; % Tolerância para o erro relativo
+    div_tol = 1e-6; % Tolerância para identificar estagnação
+    stagnation_iter = 10; % Janela de iterações para checar estagnação
+
+    previous_errors = ones(stagnation_iter, 1) * inf; % Armazena erros anteriores para detecção de estagnação
 
     for k = 1:max_iter
         x_old = x;
@@ -30,25 +29,29 @@ function sor_method()
         % Calcular erro relativo
         rel_error = norm(x - x_old) / norm(x);
 
-        % Exibir resultados para cada iteração
-        fprintf('Iteração: %d, x1: %06.4f, x2: %06.4f, x3: %06.4f, Erro relativo: %06.4f\n', k, x(1), x(2), x(3), rel_error);
+        % Exibir resultados de cada iteração
+        fprintf('Iteração: %d, ', k);
+        for ii = 1:n
+            fprintf('x%d: %06.4f, ', ii, x(ii));
+        end
+        fprintf('Erro relativo: %06.4f\n', rel_error);
 
-        % Verificar divergência
+        % Checagem de divergência
         if rel_error > previous_errors(end)
             fprintf('Divergência detectada após %d iterações.\n', k);
             break;
         end
 
-        % Atualizar erros anteriores
+        % Atualizar histórico de erros para detecção de estagnação
         previous_errors = [previous_errors(2:end); rel_error];
 
-        % Verificar estagnação
+        % Checar estagnação
         if max(previous_errors) - min(previous_errors) < div_tol
             fprintf('Estagnação detectada após %d iterações.\n', k);
             break;
         end
 
-        % Verificar se o erro relativo é menor que a tolerância
+        % Checar convergência
         if rel_error < tol
             fprintf('Convergência alcançada após %d iterações.\n', k);
             break;
